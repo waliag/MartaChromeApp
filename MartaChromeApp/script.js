845b0ee8bbe070c55ec61d000dcc57b9b7c1c8ee
Garima Walia <garima.nsit@gmail.com>
2014-11-30 10:26:14 -0500

    changes for chrome extension and bootstrap

--- MartaChromeApp/script.js
+++ MartaChromeApp/script.js
@@ -1,11 +1,21 @@
 ﻿var app = angular.module('myMartaApp', ['ngRoute']);
 
+//angular change to remove unsafe url
+app.config([
+    '$compileProvider',
+    function( $compileProvider )
+    {   
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+        // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+    }
+]);
+
 app.config(function ($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'view1.html',
         controller: 'SampleCtrl'
     })
-        .when('/view2', {
+        .when('/timings/:stationVal', {
             templateUrl: 'view2.html',
             controller: 'restCtrl'
         })
@@ -13,10 +23,12 @@ app.config(function ($routeProvider) {
 });
 
 app.controller('SampleCtrl', function ($scope) {
+    $scope.stationList = ['FivePoints', 'SandySprings'];
+    $scope.station = 'FivePoints';
 });
 
-function restCtrl($scope, $http) {
-    $scope.urlString = 'http://localhost:34025/Service1.svc/' + $scope.inputName;
+function restCtrl($scope, $http, $routeParams) {
+    $scope.urlString = 'http://localhost:34025/Service1.svc/' + $routeParams.stationVal;
     $http.get($scope.urlString).success(function (data) {
             $scope.response = data;
         });
